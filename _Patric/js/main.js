e54a51501f44045210d7afd90d7aa066885aaef3
Patric <pmorevs@gmail.com>
2026-05-21 10:57:26 +0300

    gravity fall

--- _Patric/js/main.js
+++ _Patric/js/main.js
@@ -7,8 +7,8 @@ var sensitivity = 0.5;
 
 var lvl_one_map = [
     { name: "floor", height: 2000, width: 2000, posX: 0, posY: 100, posZ: 0, rotX: 90, rotY: 0, rotZ: 0, color: "violet", opacity: 1, pattern: "url('assets/textures/grass.jpg')"},
-    { name: "ceiling", height: 2000, width: 2000, posX: 0, posY: -100, posZ: 0, rotX: 90, rotY: 0, rotZ: 0, color: "green", opacity: 0.5 },
-    { name: "right wall", height: 200, width: 2000, posX: 1000, posY: 0, posZ: 0, rotX: 0, rotY: 90, rotZ: 0, color: "blue", opacity: 0.5 },
+    // { name: "ceiling", height: 2000, width: 2000, posX: 0, posY: -350, posZ: 0, rotX: 90, rotY: 0, rotZ: 0, color: "green", opacity: 0.5 },
+    { name: "right wall", height: 700, width: 2000, posX: 1000, posY: 0, posZ: 0, rotX: 0, rotY: 90, rotZ: 0, color: "blue", opacity: 1, pattern: "url('assets/textures/wall.jpg')" },
     { name: "left wall", height: 200, width: 2000, posX: -1000, posY: 0, posZ: 0, rotX: 0, rotY: 90, rotZ: 0, color: "orange", opacity: 0.5 },
     // { name: "front wall", height: 200, width: 2000, posX: 0, posY: 0, posZ: 1000, rotX: 0, rotY: 0, rotZ: 0, color: "#ecc0d1", opacity: 0.5 },
     { name: "hinter wall", height: 200, width: 2000, posX: 0, posY: 0, posZ: -1000, rotX: 0, rotY: 0, rotZ: 0, color: "yellow", opacity: 0.5 },
@@ -48,6 +48,7 @@ let dx = dy = dz = dry = 0;
 let pressUp = pressDown = pressLeft = pressRight = 0;
 let mouseX = mouseY = 0;
 let vel = 10;
+var gravity = 9.8;
 
 function player(x, y, z, rx, ry, rz, vx, vy, vz) {
     this.x = x;
@@ -76,6 +77,9 @@ document.addEventListener("keydown", (e) => {
     if (e.code == "KeyA") {
         pressRight = pawn.vx;
     }
+    if (e.code == "Space") {
+
+    }
 });
 
 document.addEventListener("keyup", (e) => {
@@ -90,6 +94,9 @@ document.addEventListener("keyup", (e) => {
     }
     if (e.code == "KeyA") {
         pressRight = 0;
+    }
+    if (e.code == "Space") {
+
     }
 });
 
@@ -118,6 +125,7 @@ myContainer.addEventListener("click", async () => {
 function update() {
     dx = (pressLeft - pressRight)*Math.cos(pawn.ry * DEG) + (pressUp - pressDown)*Math.sin(pawn.ry * DEG);
     dz = -(pressLeft - pressRight)*Math.sin(pawn.ry * DEG) + (pressUp - pressDown)*Math.cos(pawn.ry * DEG);
+    dy = gravity;
 
     dry = mouseX * sensitivity;
     drx = mouseY * sensitivity;
@@ -128,11 +136,12 @@ function update() {
     if (lock) {
         pawn.z += dz;
         pawn.x += dx;
+        pawn.y += dy;
         pawn.ry += dry;
         pawn.rx -= drx;
     }
 
-    myWorld.style.transform = `translateZ(600px) RotateX(${pawn.rx}deg) RotateY(${pawn.ry}deg) translate3d(${-pawn.x}px, ${pawn.y}px, ${pawn.z}px) `;
+    myWorld.style.transform = `translateZ(600px) RotateX(${pawn.rx}deg) RotateY(${pawn.ry}deg) translate3d(${-pawn.x}px, ${-pawn.y}px, ${pawn.z}px) `;
 }
 
 var game = setInterval(update, 10);
